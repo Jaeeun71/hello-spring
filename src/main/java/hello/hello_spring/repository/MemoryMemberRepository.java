c68561a2044874d41b492fa413cc1dd11a6b36d3
jaeeun <mydlbxiu@ewhain.net>
2025-10-30 18:28:19 +0900

    스프링 입문 실습 8주차

---
+++ src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
@@ -0,0 +1,43 @@
+package hello.hello_spring.repository;
+
+import hello.hello_spring.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    
+    private static long sequence = 0L;
+
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    public Optional<Member> findById(long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    public List<Member> findAll(){
+        return new ArrayList<>(store.values());
+    }
+
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    public void clearStore(){
+        store.clear();
+    }
+
+
+
+
+
+}
